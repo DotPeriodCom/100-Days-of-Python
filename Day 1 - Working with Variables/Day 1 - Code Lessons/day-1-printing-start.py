1e372da4fd4a1dd4a213951e6208c929fa9300a6
Dot.Com <76796094+DotPeriodCom@users.noreply.github.com>
2021-01-04 02:18:33 -0500

    Add files via upload

---
+++ Day 1 - Working with Variables/Day 1 - Code Lessons/day-1-printing-start.py
@@ -0,0 +1,3 @@
+# The input() will get user input in console
+# The print() will print the word "Hello " and the user input
+print("Hello " + input("What is your name?"))
---
+++ Day 1 - Working with Variables/Day 1 - Code Lessons/day-1-variables-start.py
@@ -0,0 +1,9 @@
+name = "Jack"
+print(name)
+
+name = "Angela"
+print(name)
+
+user_name = input("What is your name? ")
+length1 = len(user_name)
+print(length1)
