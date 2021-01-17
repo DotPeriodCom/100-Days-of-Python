d6dc8b787451559dcdf261009571f6e91ceb13f3
Dot.Com <76796094+DotPeriodCom@users.noreply.github.com>
2021-01-16 23:17:57 -0500

    Add files via upload

---
+++ Day 1 - Working with Variables/Day 1 - Code Lessons/day-1-printing-start.py
@@ -0,0 +1,8 @@
+# Write your code below this line 👇
+print("Hello world!")
+
+# Notes
+# print("Something") = print() function
+# Strings = String of characters in paranthesis
+# SyntaxError: EOL while scanning string literal
+# Command + / = Command + Forwardslash to comment that line of code on the cursor
---
+++ Day 1 - Working with Variables/Day 1 - Code Lessons/day-1-variables-start.py
@@ -0,0 +1,17 @@
+name = "Jack"
+print(name)
+
+name = "Angela"
+print(name)
+
+# print( len( input("What is your name?") ) )
+name = input("What is your name?")
+length = len(name)
+print(length)
+
+# name = input("What is your name?")
+# print(name)
+
+# Notes
+# Variable
+# james = 0125345769
